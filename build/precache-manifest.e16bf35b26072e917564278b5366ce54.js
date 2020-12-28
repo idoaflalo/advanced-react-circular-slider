b9e299851209bd17a76ed3c5d2332d89329039c7
jsdev63 <jsdev63@yahoo.com>
2020-12-28 17:51:05 +0100

    added build version

---
+++ build/precache-manifest.e16bf35b26072e917564278b5366ce54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e66b6fb100a045c46762f3def02fbb",
+    "url": "/advanced_react-circular-slider/index.html"
+  },
+  {
+    "revision": "a72600c8704ecdb7b387",
+    "url": "/advanced_react-circular-slider/static/css/main.863b3ee0.chunk.css"
+  },
+  {
+    "revision": "f2ffe19844dadd0c428e",
+    "url": "/advanced_react-circular-slider/static/js/2.236b0ac4.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/advanced_react-circular-slider/static/js/2.236b0ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72600c8704ecdb7b387",
+    "url": "/advanced_react-circular-slider/static/js/main.efeaf3eb.chunk.js"
+  },
+  {
+    "revision": "3768955ce2787dc5abd0",
+    "url": "/advanced_react-circular-slider/static/js/runtime-main.23611954.js"
+  }
+]);
