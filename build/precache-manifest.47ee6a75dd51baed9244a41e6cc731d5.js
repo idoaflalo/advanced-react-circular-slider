a669447e990ff8ac078d0a41a19399cb4595962a
Ido Aflalo <ido.aflalo@gmail.com>
2021-11-15 20:02:07 +0200

    update build files

---
+++ build/precache-manifest.47ee6a75dd51baed9244a41e6cc731d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840571e9fc669cf18178f00258c0c5c8",
+    "url": "/advanced-react-circular-slider/index.html"
+  },
+  {
+    "revision": "61bb4e69d9bcf8eec997",
+    "url": "/advanced-react-circular-slider/static/css/main.3c74f173.chunk.css"
+  },
+  {
+    "revision": "9cec17ad40805df4cb11",
+    "url": "/advanced-react-circular-slider/static/js/2.f957d2cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/advanced-react-circular-slider/static/js/2.f957d2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61bb4e69d9bcf8eec997",
+    "url": "/advanced-react-circular-slider/static/js/main.ee4a1da8.chunk.js"
+  },
+  {
+    "revision": "d83c5a89468d2e10b1d7",
+    "url": "/advanced-react-circular-slider/static/js/runtime-main.e8508445.js"
+  },
+  {
+    "revision": "6fb23c12140a7e53d35b1494544f3040",
+    "url": "/advanced-react-circular-slider/static/media/Telex.6fb23c12.ttf"
+  }
+]);
