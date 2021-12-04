210e4bf8ff8521ef964fedb33b6b212bb1e94574
Ido Aflalo <ido.aflalo@gmail.com>
2021-12-04 21:46:29 +0200

    add types

---
+++ types/index.d.ts
@@ -0,0 +1,51 @@
+import React from "react";
+import { ReactElement } from "react";
+
+declare module "react-d3-components" {
+  export class CircularSlider extends React.Component<CircularSliderProps & any, any> {}
+
+  interface CircularSliderProps {
+    label?: string;
+    width?: number;
+    direction?: number;
+    min?: number;
+    max?: number;
+    step?: number;
+    limit?: number;
+    labelTop?: string;
+    labelStep?: number;
+    offsetAngle?: number;
+    knobSize?: number;
+    doubleLineColor?: string | null;
+    doubleLineType?: string;
+    knobColor?: string;
+    knobPosition?: string;
+    hideKnob?: boolean;
+    knobDraggable?: boolean;
+    knobEl?: ReactElement | null;
+    labelColor?: string;
+    labelOffset?: number;
+    secondaryLabelColor?: string;
+    roundLabelColor?: string;
+    labelBottom?: string;
+    labelFontSize?: string;
+    roundLabelFontSize?: string;
+    valueFontSize?: string;
+    appendToValue?: string;
+    renderLabelValue?: ReactElement | null;
+    prependToValue?: string;
+    verticalOffset?: string;
+    hideLabelValue?: boolean;
+    progressLineCap?: "butt" | "round" | "square" | "inherit" | undefined;
+    progressColorFrom?: string;
+    progressColorTo?: string;
+    progressSize?: number;
+    trackColor?: string;
+    trackSize?: number;
+    data?: any[];
+    dataIndex?: number;
+    activeLabelColor?: string;
+    magentTolerance?: number;
+    onChange?: Function;
+  }
+}
